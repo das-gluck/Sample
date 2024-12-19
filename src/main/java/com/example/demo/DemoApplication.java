ce5bf542b717754d0815368e23f11221db89fc50
das-gluck <05gaurav03@gmail.com>
2024-12-19 22:24:15 +0530

    new line added

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,10 +9,10 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("This is Demo project...");
-		System.out.println("Sample1");
-		System.out.println("Sample2");
-		System.out.println("Sample1");
-		System.out.println("Sample2");
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
 	}
 
 }
