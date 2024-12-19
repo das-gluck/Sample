e9832925c58604bfa41e5bd8fb72899fb3a85b97
Gaurav Verma 240850320020 <113839475+das-gluck@users.noreply.github.com>
2024-12-19 11:33:32 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,10 +9,6 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("This is Demo project...");
-		System.out.println("Sample1");
-		System.out.println("Sample2");
-		System.out.println("Sample1");
-		System.out.println("Sample2");
 		System.out.println("AG made changes");
 		System.out.println("Ag made changes second time");
 
